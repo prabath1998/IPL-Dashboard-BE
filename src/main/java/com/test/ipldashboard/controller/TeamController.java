ded133a02ae969809e2ded66609c168dc32d3209
prabath1998 <prabhathudayanga1998@gmail.com>
2022-07-08 11:46:41 +0530

    get matches by team name api added

--- src/main/java/com/test/ipldashboard/controller/TeamController.java
+++ src/main/java/com/test/ipldashboard/controller/TeamController.java
@@ -1,6 +1,7 @@
 package com.test.ipldashboard.controller;
 
 import com.test.ipldashboard.model.Team;
+import com.test.ipldashboard.repository.MatchRepository;
 import com.test.ipldashboard.repository.TeamRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -13,9 +14,13 @@ public class TeamController {
     @Autowired
     private TeamRepository teamRepository;
 
+    @Autowired
+    private MatchRepository matchRepository;
+
     @GetMapping("/team/{teamName}")
     public Team getTeam(@PathVariable String teamName){
         Team team = this.teamRepository.findByTeamName(teamName);
+         team.setMatches(this.matchRepository.getByTeam1OrTeam2(teamName,teamName));
         return team;
     }
 }
