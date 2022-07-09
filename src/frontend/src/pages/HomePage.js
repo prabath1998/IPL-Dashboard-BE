2a8d58d18c119615f6a9d3a1b28bbc4a6c7a7dd6
prabath1998 <prabhathudayanga1998@gmail.com>
2022-07-09 10:20:08 +0530

    minor changes

--- src/frontend/src/pages/HomePage.js
+++ src/frontend/src/pages/HomePage.js
@@ -9,7 +9,7 @@ export const HomePage = () => {
     useEffect(
         () => {
          const fetchAllTeams = async () => {
-            const response = await fetch(` http://localhost:8080/team`);
+            const response = await fetch(`http://localhost:8080/team`);
             const data = await response.json();
             setTeams(data);
 
@@ -24,7 +24,7 @@ export const HomePage = () => {
     return (
         <div className="HomePage">
             <div className="header-section">
-                <h1 className="app-name">Java Brains IPL Dashboard</h1>
+                <h1 className="app-name">IPL Dashboard</h1>
             </div>
             <div className="team-grid">
                 { teams.map(team => <TeamTile key={team.id} teamName={team.teamName} />)}
